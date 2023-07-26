d7f71533c711e8e891718bbf2db9ebf097b62d34
Ayush Nandi <110190468+ayushnandi@users.noreply.github.com>
2023-07-26 11:07:41 +0530

    Create Diplay Message.java
    1. display msgs

---
+++ Milestone 1/Lecture 2 Getting Started With Java/Diplay Message.java
@@ -0,0 +1,44 @@
+/*
+Display Message
+
+Problem : Write a program to display a welcome message on the screen.
+
+Welcome Message:
+
+Welcome to Java course!
+Hello
+Ninja
+
+Output format:
+
+Print the welcome message.
+
+Note:-
+
+You don't need to return anything. Just print the desired output.
+
+Constraints:
+
+Time Limit: 1-sec
+
+Sample Output 1:
+
+Welcome to Java course!
+Hello
+Ninja
+
+Explanation Of Sample Input 1:
+Printed the welcome message.
+*/
+ 
+// Solution :
+
+public class Solution {
+    public static void printDisplayMessage() {
+        // Write your code heres
+        System.out.println("Welcome to Java course!");
+        System.out.println("Hello");
+        System.out.println("Ninja");
+
+    }
+}
