08c4b0dd603d2161d8d2d57581559a74b4ee5226
Ayush Nandi <110190468+ayushnandi@users.noreply.github.com>
2023-07-27 23:12:43 +0530

    level 3 :

---
+++ Milestone 1/Lecture 3  How is Data Stored/Multiply Floating Numbers.java
@@ -0,0 +1,42 @@
+Multiply Floating Numbers
+
+/*
+Write a code which takes two integers "F1" and "F2" as inputs and print their product .
+
+Input Format
+
+The first and only line contains two two integers, ‘F1’, and 'F2',
+
+Output format:
+
+Print the product of these numbers.
+
+Constraints:
+1<= F1, F2 <= 100000
+
+Time Limit: 1-sec
+
+Sample Input 1:
+
+123456789  123456789
+
+Sample Output 1:
+
+15241578750190521
+
+Sample Input 2:
+
+123456 123456
+
+Sample Output 2:
+
+15241383936
+*/
+
+public class Solution {
+    public static double multiplyFloatingNumbers(double f1, double f2) {
+        // Write your code here
+        double a = (f1*f2) ;
+        return a ;
+    }
+}
---
+++ Milestone 1/Lecture 3  How is Data Stored/Print ASCII Value.java
@@ -0,0 +1,64 @@
+/*
+Print ASCII Value
+
+Given a character, you need to print the ASCII value of that character.
+
+Example:
+
+Input: ch = ‘a’
+
+Output: 97
+
+Explanation: ASCII value of ‘a’ is 97.
+
+Input Format
+
+The first and only line contains a character, ‘ch’.
+
+Output format:
+
+Print the ASCII value of the character.
+
+Constraints:
+
+1 <= TEST <= 10
+All characters are either lowercase or uppercase letters.
+
+Time Limit: 1-sec
+
+Sample Input 1:
+
+‘a’
+
+Sample Output 1:
+
+97
+
+Explanation Of Sample Input 1:
+For test case 1:
+
+Input: ch = ‘a’
+
+Output: 97
+
+Explanation: ASCII value of ‘a’ is 97.
+
+Sample Input 2:
+
+‘B’
+
+Sample Output 2:
+
+66
+*/
+
+import java.util.Scanner;
+public class Solution {
+    public static int calculateASCII(char ch) {
+        // Write your code here
+        
+        // ch = 'a' ;
+        int i = ch ;
+        return i ;
+    }
+}
