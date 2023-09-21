1c4845342fa46277852ba65aa880aaacc525d576
Ayush Nandi <110190468+ayushnandi@users.noreply.github.com>
2023-09-21 23:20:52 +0530

    Merge Sort - Problem Statement

---
+++ Milestone 3/Recursion - II/Merge Sort - Problem Statement.java
@@ -0,0 +1,76 @@
+/*
+Merge Sort - Problem Statement
+
+You are given the starting 'l' and the ending 'r' positions of the array 'ARR'.
+
+
+You must sort the elements between 'l' and 'r'.
+
+
+Note
+Change in the input array itself. So no need to return or print anything.
+Example
+Input ‘N’ = 7,
+'ARR' = [2, 13, 4, 1, 3, 6, 28]
+
+Output [1 2 3 4 6 13 28]
+
+Explanation After applying 'merge sort' on the input array, the output is [1 2 3 4 6 13 28].
+Input format 
+The first line contains an integer em'N'em representing the size of the arraylist.
+
+The second line contains 'N' single space-separated integers representing the elements in the arraylist.
+Output format 
+You don't need to return anything. In the output, you will see the array after you do the modification.
+
+Sample Input 1
+
+7
+2 13 4 1 3 6 28
+
+Sample Output 1
+
+1 2 3 4 6 13 28
+
+Explanation of Sample Output 1
+After applying 'merge sort' on the input array, the output is [1 2 3 4 6 13 28].
+
+Sample Input 2
+
+5
+9 3 6 2 0
+
+Sample Output 2
+
+0 2 3 6 9
+
+Explanation of Sample Output 2
+After applying 'merge sort' on the input array, the output is [0 2 3 6 9].
+
+Constraints 
+
+1 = N = 10^3
+0 = ARR[i] = 10^9
+
+*/
+
+// Solution :
+
+public class solution {
+
+	public static void mergeSort(int[] input){
+		// Write your code here
+		if (input.length==0){
+			return;
+		}
+		for ( int i = 1 ; i <= input.length ; i ++ ){
+			int v = input[0];  
+			if ( v >= input [i] ){
+				int temp;
+				temp = input[0];
+				input[0]= input[i];
+				input[i]= temp;
+			}
+		}
+	}
+}
